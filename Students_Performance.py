9749924e160f68386fd6207e291ce4a124f37fe2
Patryk <hamerlikpatryk1@gmail.com>
2022-08-24 17:24:11 +0200

    Small fixing

---
+++ Students_Performance.py
@@ -0,0 +1,12 @@
+#!/home/DS_Projects/Students_Performance_venv/bin/activate
+#source Students_Performance_venv/bin/activate
+#simple fibonacci for initial commit
+
+#Modulo function -> check if number is even or odd
+def simple_mod(n): 
+    if n%2 == 0:
+        return "Even"
+    else:
+        return "Odd"
+
+print(simple_mod(21)) 
