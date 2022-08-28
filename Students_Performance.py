1a86d468dae0548e76073770dca8a4572a35ed62
Patryk <hamerlikpatryk1@gmail.com>
2022-08-28 17:08:47 +0200

    reading csv

--- Students_Performance.py
+++ Students_Performance.py
@@ -1,12 +1,20 @@
 #!/home/DS_Projects/Students_Performance_env/bin/activate
-#source Students_Performance_venv/bin/activate
-#simple fibonacci for initial commit
+# source Students_Performance_venv/bin/activate
+# simple fibonacci for initial commit
+# pip install -r requirements.txt
 
-#Modulo function -> check if number is even or odd
-def simple_mod(n): 
-    if n%2 == 0:
-        return "Even"
-    else:
-        return "Odd"
+# Modulo function -> check if number is even or odd
+import numpy as np
+import pandas as pd
 
-print(simple_mod(21)) 
+# get data from csv file
+students_data = pd.read_csv('StudentsPerformance.csv', sep=',', header= None)
+students_array = np.array(students_data)
+print(students_array)
+
+# sorting the array -> chose column/row/and sorting algorithm
+sorted_grades = np.sort(students_array) #axis = 0, kind = 'quicksort', order = 5)
+print(sorted_grades)
+print(type(sorted_grades))
+
+pd.DataFrame(sorted_grades).to_csv('grades_data.csv')
