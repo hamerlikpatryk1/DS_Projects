728d8eda989816efb39c647036dec9e00b77195f
Patryk <hamerlikpatryk1@gmail.com>
2022-08-30 14:56:52 +0200

    plot fix

--- Students_Performance.py
+++ Students_Performance.py
@@ -1,12 +1,11 @@
-#Interactive mode for plotting 
-# %%
-
+# %% 
 #!/home/DS_Projects/Students_Performance_env/bin/activate
-# source Students_Performance_venv/bin/activate
+#source Students_Performance_env/bin/activate
 # pip install -r requirements.txt
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+import numpy as np
 
 sns.set_theme()
 
@@ -25,8 +24,8 @@ print(grades)
 # filtering by value #loc
 # czy zrobić z tego funkcję i do każdej kolumny osobno?
 good_grades = grades.loc[(grades['math score'] >= 66) &
-                         (grades['reading score']>= 66) &
-                         (grades['writing score']>= 66)]  
+                         (grades['reading score'] >= 66) &
+                         (grades['writing score'] >= 66)]
 
 """ STATISTIC
 math_stat = grades['math score'].describe()
@@ -35,18 +34,13 @@ reading_mean = grades['reading score'].mean()
 reading_median = grades['reading score'].quantile(q = 0.50)
 print('Mean of the reading score = ', reading_mean, ', median = ', reading_median) """
 
-""" sns.relplot(
-    data = good_grades,
-    x = "test preparation course", y = "math score", col = "gender",
-) """
-
 # plotting data
+sns.relplot(data=grades, x='reading score', y='math score', hue ='test preparation course')
 sns.relplot(x="math score", y="reading score", data=good_grades)
+sns.displot(good_grades, x="math score", hue="gender", element="step")
 plt.show()
 
 # write to csv
-good_grades.to_csv('grades_data.csv', index=False) 
-
-
+good_grades.to_csv('grades_data.csv', index=False)
 
 # %%
