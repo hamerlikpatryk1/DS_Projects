3010706f7538c6cf3c39f14ce0fc7f6e1ebca426
Patryk <hamerlikpatryk1@gmail.com>
2022-08-30 12:58:50 +0200

    statistic && plotting data

--- Students_Performance.py
+++ Students_Performance.py
@@ -1,28 +1,52 @@
+#Interactive mode for plotting 
+# %%
+
 #!/home/DS_Projects/Students_Performance_env/bin/activate
 # source Students_Performance_venv/bin/activate
-# simple fibonacci for initial commit
 # pip install -r requirements.txt
-
-# Modulo function -> check if number is even or odd
-
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+sns.set_theme()
 
 # get data from csv file
 students_data = pd.read_csv('StudentsPerformance.csv')
-print(students_data.to_string())
-print(type(students_data))
+#print(students_data.to_string())
+#print(type(students_data))
 
 # sorting the array -> chose column/row/and sorting algorithm
-grades = students_data.sort_values(by = "math score", ascending = False, kind = 'mergesort') #axis = 0, kind = 'quicksort', order = 5)
+grades = students_data.sort_values(by = "math score", ascending = False, kind = 'mergesort')
 print(grades)
 
 # simple filtering by column
 # grades = grades.filter(items= ['math score', 'reading score', 'writing score']) 
 
 # filtering by value #loc
+# czy zrobić z tego funkcję i do każdej kolumny osobno?
 good_grades = grades.loc[(grades['math score'] >= 66) &
                          (grades['reading score']>= 66) &
-                         (grades['writing score']>= 66)] 
+                         (grades['writing score']>= 66)]  
+
+""" STATISTIC
+math_stat = grades['math score'].describe()
+print(math_stat)
+reading_mean = grades['reading score'].mean()
+reading_median = grades['reading score'].quantile(q = 0.50)
+print('Mean of the reading score = ', reading_mean, ', median = ', reading_median) """
+
+""" sns.relplot(
+    data = good_grades,
+    x = "test preparation course", y = "math score", col = "gender",
+) """
+
+# plotting data
+sns.relplot(x="math score", y="reading score", data=good_grades)
+plt.show()
 
 # write to csv
-good_grades.to_csv('grades_data.csv', index=False)
+good_grades.to_csv('grades_data.csv', index=False) 
+
+
+
+# %%
