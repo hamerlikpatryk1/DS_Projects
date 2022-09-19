ead2793d076bfbdd0fd6681c717c9bd41c340bd5
Patryk <hamerlikpatryk1@gmail.com>
2022-09-19 11:58:17 +0200

    functions added

--- Students_Performance.py
+++ Students_Performance.py
@@ -1,4 +1,3 @@
-# %% 
 #!/home/DS_Projects/Students_Performance_env/bin/activate
 #source Students_Performance_env/bin/activate
 # pip install -r requirements.txt
@@ -7,26 +6,38 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 
-sns.set_theme()
+# sns.set_theme()
 
 # get data from csv file
-students_data = pd.read_csv('StudentsPerformance.csv')
-#print(students_data.to_string())
-#print(type(students_data))
+def readCSV(filename):
+    students_data = pd.read_csv(filename)
+    return students_data
 
-# sorting the array -> chose column/row/and sorting algorithm
-grades = students_data.sort_values(by = "math score", ascending = False, kind = 'mergesort')
-print(grades)
+# sorts data by mathscore
+def mathsort(students_data):
+    grades = students_data.sort_values(by = "math score", ascending = False, kind = 'mergesort')
+    print(grades)
+    return grades
 
-# simple filtering by column
-# grades = grades.filter(items= ['math score', 'reading score', 'writing score']) 
-
-# filtering by value #loc
-# czy zrobić z tego funkcję i do każdej kolumny osobno?
-good_grades = grades.loc[(grades['math score'] >= 66) &
+# filtering the highest scores #loc 
+def bestgrade(grades):
+    good_grades = grades.loc[(grades['math score'] >= 66) &
                          (grades['reading score'] >= 66) &
                          (grades['writing score'] >= 66)]
+    return good_grades
 
+#def bahwithtest(grades):
+#    bahelortest = grades.loc
+#    return bahelortest
+
+students_data = readCSV('StudentsPerformance.csv')
+
+grades = mathsort(students_data)
+# sorting the array -> chose column/row/and sorting algorithm
+good_grades = bestgrade(grades)
+
+# 4 metryki -> 1 best grades, 2 mean of bachelores with test complete , 3 mean + median of test complete and uncompleted, 4 mean + median of males/females with standard lunch 
+#   
 """ STATISTIC
 math_stat = grades['math score'].describe()
 print(math_stat)
@@ -35,12 +46,10 @@ reading_median = grades['reading score'].quantile(q = 0.50)
 print('Mean of the reading score = ', reading_mean, ', median = ', reading_median) """
 
 # plotting data
-sns.relplot(data=grades, x='reading score', y='math score', hue ='test preparation course')
-sns.relplot(x="math score", y="reading score", data=good_grades)
-sns.displot(good_grades, x="math score", hue="gender", element="step")
-plt.show()
+# sns.relplot(data=grades, x='reading score', y='math score', hue ='test preparation course')
+# sns.relplot(x="math score", y="reading score", data=good_grades)
+# sns.displot(good_grades, x="math score", hue="gender", element="step")
+# plt.show()
 
 # write to csv
 good_grades.to_csv('grades_data.csv', index=False)
-
-# %%
