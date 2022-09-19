907a073d3b7ef8cb0508a2ca20ec5416044f76bc
Patryk <hamerlikpatryk1@gmail.com>
2022-09-19 14:38:58 +0200

    4 metrics

--- Students_Performance.py
+++ Students_Performance.py
@@ -46,6 +46,27 @@ def courseuncompleted(grades):
     nocourse = grades.loc[(grades['test preparation course'] == "none")]
     return nocourse
 
+# Grade of men with lunch 
+def menwithlunch(grades):
+    menlunch = grades.loc[(grades['gender'] == "male") &
+                        (grades['lunch'] == 'standard')]
+    return menlunch
+# Grade of men without lunch 
+def menwithoutlunch(grades):
+    mennolunch = grades.loc[(grades['gender'] == "male") &
+                        (grades['lunch'] == 'free/reduced')]
+    return mennolunch
+# Grade of women with lunch 
+def womenwithlunch(grades):
+    womenlunch = grades.loc[(grades['gender'] == "female")&
+                        (grades['lunch'] == 'standard')]
+    return womenlunch
+# Grade of women without lunch 
+def womenwithoutlunch(grades):
+    womennolunch = grades.loc[(grades['gender'] == "female")&
+                            (grades['lunch'] =='free/reduced')]
+    return womennolunch
+
 # mean of bachwithtest
 def bachmean(grades, score):
     bachtestmean = bachwithtest(grades)[score].mean()
@@ -71,6 +92,40 @@ def coursemedian(grades, score):
 def nocoursemedian(grades, score):
     nocoursemean = courseuncompleted(grades)[score].quantile(q = 0.50)
     return nocoursemean
+
+# mean/median of men/women with/without lunch
+def menlunchmedian(grades, score):
+    menlunch = menwithlunch(grades)[score].quantile(q = 0.50)
+    return menlunch
+
+def menlunchmean(grades, score):
+    menlunch = menwithlunch(grades)[score].mean()
+    return menlunch
+
+def mennolunchmedian(grades, score):
+    mennolunch = menwithoutlunch(grades)[score].quantile(q = 0.50)
+    return mennolunch
+
+def mennolunchmean(grades, score):
+    mennolunch = menwithoutlunch(grades)[score].mean()
+    return mennolunch
+
+def womenlunchmedian(grades, score):
+    womanlunch = womenwithlunch(grades)[score].quantile(q = 0.50)
+    return womanlunch
+
+def womenlunchmean(grades, score):
+    womanlunch = womenwithlunch(grades)[score].mean()
+    return womanlunch
+
+def womennolunchmedian(grades, score):
+    womannolunch = womenwithoutlunch(grades)[score].quantile(q = 0.50)
+    return womannolunch
+
+def womennolunchmean(grades, score):
+    womannolunch = womenwithoutlunch(grades)[score].mean()
+    return womannolunch
+
 students_data = readCSV('StudentsPerformance.csv')
 
 # Main variable -> sorted grades
@@ -79,7 +134,8 @@ grades = mathsort(students_data)
 # 1 metric 
 good_grades = bestgrade(grades)
 
-#2 metric -> mean of bachelores + completed course
+# 2 metric
+# mean of bachelores + completed course
 print(
     '\nBachelors children math mean = ', bachmean(grades, 'math score'),
     '\nBachelors children reading mean = ',bachmean(grades,'reading score'),
@@ -101,7 +157,7 @@ print (
     '\nNone course completed math mean = ', nocoursemean(grades, 'math score'),
     '\nNone course completed reading mean =', nocoursemean(grades,'reading score'),
     '\nNone course completed writing mean = ', nocoursemean(grades,'writing score') )
-#Median
+# Median
 print ( 
     '\nCourse completed math median = ', coursemedian(grades, 'math score'),
     '\nCourse completed reading median =', coursemedian(grades,'reading score'),
@@ -110,15 +166,45 @@ print (
     '\nNone course completed math median = ', nocoursemedian(grades, 'math score'),
     '\nNone course completed reading median =', nocoursemedian(grades,'reading score'),
     '\nNone course completed writing median = ', nocoursemedian(grades,'writing score') )
+ 
+# Dodać plotting i zapisać go do pliku
 
-# 4 metryki -> 1 best grades, 2 mean of bachelores with test complete -> compare with high school and completed coures , 3 mean + median of test complete and uncompleted, 4 mean + median of males/females with standard lunch 
-#   
-# STATISTIC
-# math_stat = grades['math score'].describe()
-# print(math_stat)
-# reading_mean = grades['reading score'].mean()
-# reading_median = grades['reading score'].quantile(q = 0.50)
-# print('Mean of the reading score = ', reading_mean, ', median = ', reading_median) """
+# 4 metric
+# Men with lunch 
+print ( 
+    '\nMen with lunch math mean = ', menlunchmean(grades, 'math score'),
+    '\nMen with lunch reading mean =', menlunchmean(grades,'reading score'),
+    '\nMen with lunch writing mean = ', menlunchmean(grades,'writing score'),
+    '\nMen with lunch math median = ', menlunchmedian(grades, 'math score'),
+    '\nMen with lunch reading median =', menlunchmedian(grades,'reading score'),
+    '\nMen with lunch writing median = ', menlunchmedian(grades,'writing score'))
+# Woman With lunch 
+print ( 
+    '\nWomen with lunch math mean = ', womenlunchmean(grades, 'math score'),
+    '\nWomen with lunch reading mean =', womenlunchmean(grades,'reading score'),
+    '\nWomen with lunch writing mean = ', womenlunchmean(grades,'writing score'),
+    '\nWomen with lunch math median = ', womenlunchmedian(grades, 'math score'),
+    '\nWomen with lunch reading median =', womenlunchmedian(grades,'reading score'),
+    '\nWomen with lunch writing median = ', womenlunchmedian(grades,'writing score'))
+# Men without lunch
+print ( 
+    '\nMen with lunch math mean = ', mennolunchmean(grades, 'math score'),
+    '\nMen with lunch reading mean =', mennolunchmean(grades,'reading score'),
+    '\nMen with lunch writing mean = ', mennolunchmean(grades,'writing score'),
+    '\nMen with lunch math median = ', mennolunchmedian(grades, 'math score'),
+    '\nMen with lunch reading median =', mennolunchmedian(grades,'reading score'),
+    '\nMen with lunch writing median = ', mennolunchmedian(grades,'writing score'))
+# Woman without lunch
+print ( 
+    '\nWomen with lunch math mean = ', womennolunchmean(grades, 'math score'),
+    '\nWomen with lunch reading mean =', womennolunchmean(grades,'reading score'),
+    '\nWomen with lunch writing mean = ', womennolunchmean(grades,'writing score'),
+    '\nWomen with lunch math median = ', womennolunchmedian(grades, 'math score'),
+    '\nWomen with lunch reading median =', womennolunchmedian(grades,'reading score'),
+    '\nWomen with lunch writing median = ', womennolunchmedian(grades,'writing score'))
+    
+# 4 metryki -> 1 best grades, 2 mean of bachelores with test complete -> compare with high school and completed coures , 
+# 3 mean + median of test complete and uncompleted, 4 mean + median of males/females with standard lunch + without lunch 
 
 # plotting data
 # sns.relplot(data=grades, x='reading score', y='math score', hue ='test preparation course')
